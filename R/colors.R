a3a689ceb18323b457802d2daedab957bca0b799
s-jessen <116815282+s-jessen@users.noreply.github.com>
2024-06-28 11:08:39 +0200

    statistics and M/F figures

--- R/colors.R
+++ R/colors.R
@@ -3,3 +3,6 @@ for_o_color <- "#697f95"
 sym_color <- "#959664"
 placebo_color <- "#f2e8c7"
 mannitol_color <- "gray"
+
+male_color <- "#5f95ce"
+female_color <- "#eeaf52"
---
+++ R/functions.R
@@ -0,0 +1,118 @@
+barplot <- function(df, variable, ylab) {
+
+
+    #Calculate visually pleasing y-axis limits w. padding for p-values
+    y_values <- df %>% filter(treatment != "pla") %>% pull(!!sym(variable))
+
+    y_breaks <- scales::pretty_breaks(n = 5)(y_values)
+
+    y_min <- min(y_breaks)
+
+    y_max <- max(y_breaks*1.25)
+
+    #Plot figure
+    fig <- df %>%
+        dplyr::filter(treatment != "pla") %>%
+        ggplot2::ggplot(aes(x = treatment, y = !!sym(variable), color = treatment, fill = treatment))+
+        geom_point(size = 4, shape = 16, alpha = 0.5)+
+        geom_bar(stat = "summary", fun = "mean")+
+        geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.25)+
+        theme(
+            panel.background = element_blank(),
+            panel.border = element_rect(colour = "black", fill=NA, linewidth = 0.25),
+            panel.grid.minor=element_blank(),
+            panel.grid.major = element_blank(),
+            plot.background = element_rect(fill = "white"),
+            axis.line = element_blank(),
+            axis.text = element_text(color = "black"),
+            #axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+            text = element_text(size = 8, family="Source Sans Pro", color = "black"),
+            axis.title = element_text(size = 8, family="Source Sans Pro"),
+            legend.title = element_blank(),
+            legend.key = element_blank(),
+            legend.key.size = (unit(3, "mm")),
+            legend.position = "none",
+            title = element_text(size = 6),
+            aspect.ratio = 1,
+            strip.background = element_blank()
+        )+
+        scale_color_manual(
+            values = c("for_i" = for_i_color, "for_o" = for_o_color, "sym" = sym_color, man = mannitol_color),
+            labels = c(for_i = "FOR inhaled", for_o = "FOR oral", sym = "Symbicort", man = "Mannitol")
+        )+
+        scale_fill_manual(
+            values = c("for_i" = for_i_color, "for_o" = for_o_color, "sym" = sym_color, man = mannitol_color),
+            labels = c(for_i = "FOR inhaled", for_o = "FOR oral", sym = "Symbicort", man = "Mannitol")
+        )+
+        labs(x = "", y = ylab)+
+        scale_x_discrete(labels = c(for_i = "FOR\ninhaled", for_o = "FOR\noral", sym = "Symbicort", man = "Mannitol")) +
+        scale_y_continuous(limits = c(y_min, y_max), breaks = y_breaks)
+
+    print(fig)
+
+}
+
+barplot_sex <- function(df, variable, ylab) {
+
+
+    #Calculate visually pleasing y-axis limits w. padding for p-values
+    y_values <- df %>% filter(treatment != "pla") %>% pull(!!sym(variable))
+
+    y_breaks <- scales::pretty_breaks(n = 5)(y_values)
+
+    y_min <- min(y_breaks)
+
+    y_max <- max(y_breaks*1.25)
+
+    #Plot figure
+    fig <- df %>%
+        dplyr::filter(treatment != "pla") %>%
+        ggplot2::ggplot(aes(x = treatment, y = !!sym(variable), group = sex))+
+        geom_point(aes(color = treatment, shape = sex),
+                   size = 4,
+                   alpha = 0.5,
+                   position = position_dodge(width = 1)
+        )+
+        geom_bar(aes(fill = treatment),
+                 stat = "summary",
+                 fun = "mean",
+                 position = position_dodge(width = 1)
+        )+
+        geom_hline(yintercept = 0, linetype = "dashed", linewidth = 0.25)+
+        theme(
+            panel.background = element_blank(),
+            panel.border = element_rect(colour = "black", fill=NA, linewidth = 0.25),
+            panel.grid.minor=element_blank(),
+            panel.grid.major = element_blank(),
+            plot.background = element_rect(fill = "white"),
+            axis.line = element_blank(),
+            axis.text = element_text(color = "black"),
+            #axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+            text = element_text(size = 8, family="Source Sans Pro", color = "black"),
+            axis.title = element_text(size = 8, family="Source Sans Pro"),
+            legend.title = element_blank(),
+            legend.key = element_blank(),
+            legend.key.size = (unit(3, "mm")),
+            legend.position = "right",
+            title = element_text(size = 6),
+            aspect.ratio = 1,
+            strip.background = element_blank()
+        )+
+        scale_color_manual(
+            values = c("for_i" = for_i_color, "for_o" = for_o_color, "sym" = sym_color, man = mannitol_color),
+            labels = c(for_i = "FOR inhaled", for_o = "FOR oral", sym = "Symbicort", man = "Mannitol")
+        )+
+        scale_fill_manual(
+            values = c("for_i" = for_i_color, "for_o" = for_o_color, "sym" = sym_color, man = mannitol_color),
+            labels = c(for_i = "FOR inhaled", for_o = "FOR oral", sym = "Symbicort", man = "Mannitol")
+        )+
+        scale_shape_manual(
+            values = c("M" = 16, "W" = 15),
+            labels = c("M" = "Males", "W" = "Females")
+        )+
+        labs(x = "", y = "ylab")+
+        scale_x_discrete(labels = c(for_i = "FOR\ninhaled", for_o = "FOR\noral", sym = "Symbicort", man = "Mannitol"))
+
+    print(fig)
+
+}
